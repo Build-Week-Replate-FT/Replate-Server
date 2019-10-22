49f709675dcc2e8ae985ca2f96b75d436ea39059
Jason Sonnichsen <Harliquinn@gmail.com>
2019-10-22 17:53:59 -0400

    Can't push to Heroku for some reason

--- StartHere/src/main/java/com/lambdaschool/starthere/services/VolunteerServiceImpl.java
+++ StartHere/src/main/java/com/lambdaschool/starthere/services/VolunteerServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Loggable
-@Service(value = "VolunteerService")
+@Service(value = "volunteerService")
 public class VolunteerServiceImpl implements VolunteerService
 {
     @Autowired
