e88f7736dd0df8f44264d1ecfbfc7e102c890514
Jason Sonnichsen <Harliquinn@gmail.com>
2019-10-24 18:51:13 -0400

    Further testing in place including save for Pickups which took some time.

--- StartHere/src/test/java/com/lambdaschool/starthere/services/BusinessServiceImplUnitTest.java
+++ StartHere/src/test/java/com/lambdaschool/starthere/services/BusinessServiceImplUnitTest.java
@@ -1,6 +1,8 @@
 package com.lambdaschool.starthere.services;
 
 import com.lambdaschool.starthere.StartHereApplication;
+import com.lambdaschool.starthere.models.User;
+import com.lambdaschool.starthere.models.UserRoles;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -11,13 +13,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+
 import static junit.framework.TestCase.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = StartHereApplication.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class BusinessServiceImplTest
+public class BusinessServiceImplUnitTest
 {
+    @Autowired
+    UserService userService;
+
     @Autowired
     BusinessService businessService;
 
@@ -38,5 +46,21 @@ public class BusinessServiceImplTest
     {
         System.out.println();
         assertEquals(1, businessService.findAll(Pageable.unpaged()).size());
+    }
+
+    @Test
+    public void B_save()
+    {
+        ArrayList<UserRoles> datas = new ArrayList<>();
+        User u1 = new User("business", "tiger@school.lambda",
+                "ILuvMath!",
+                datas, "Tiger Mitchell", "1 Main st.", "Trenton", "NJ", "07222", "business.com");
+        User saveU2 = userService.save(u1);
+
+        System.out.println("*** DATA ***");
+        System.out.println(saveU2);
+        System.out.println("*** DATA ***");
+
+        assertEquals("tiger@school.lambda", saveU2.getEmail());
     }
 }
--- StartHere/src/test/java/com/lambdaschool/starthere/services/PickupServiceImplUnitTest.java
+++ StartHere/src/test/java/com/lambdaschool/starthere/services/PickupServiceImplUnitTest.java
@@ -1,6 +1,10 @@
 package com.lambdaschool.starthere.services;
 
 import com.lambdaschool.starthere.StartHereApplication;
+import com.lambdaschool.starthere.models.Business;
+import com.lambdaschool.starthere.models.Pickup;
+import com.lambdaschool.starthere.models.User;
+import com.lambdaschool.starthere.models.UserRoles;
 import net.bytebuddy.asm.Advice;
 import org.junit.After;
 import org.junit.Before;
@@ -12,6 +16,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+
 import static junit.framework.TestCase.assertEquals;
 
 @RunWith(SpringRunner.class)
@@ -24,6 +31,12 @@ public class PickupServiceImplUnitTest
 
     @Autowired
     VolunteerService volunteerService;
+
+    @Autowired
+    BusinessService businessService;
+
+    @Autowired
+    UserService userService;
 
     @Before
     public void setUp() throws Exception
@@ -40,7 +53,43 @@ public class PickupServiceImplUnitTest
     @Test
     public void A_findAllPageable()
     {
-        System.out.println();
         assertEquals(3, pickupService.findAll(Pageable.unpaged()).size());
+    }
+
+    @Test
+    public void B_save()
+    {
+        ArrayList<UserRoles> roles = new ArrayList<>();
+
+        User u1 = new User("business", "tiger@school.lambda",
+            "ILuvMath!",
+                roles, "Tiger Mitchell", "1 Main st.", "Trenton", "NJ", "07222", "business.com");
+        u1=userService.save(u1);
+
+        Business b1 = new Business(new ArrayList<Pickup>(),u1);
+        b1.setUserid(u1.getUserid());
+        b1=businessService.save(b1);
+
+        Pickup p1 = new Pickup("Marshmallows", 1, "Bushel", "10 Pumpkinpatch ln.", "Columbus", "OH", "45678", null,b1);
+        Pickup saveP1 = pickupService.save(p1);
+
+        assertEquals("Marshmallows", saveP1.getFoodtype());
+    }
+
+    @Test
+    public void C_update()
+    {
+
+    }
+
+    @Test
+    public void D_delete()
+    {
+
+    }
+
+    @Test
+    public void E_updatePickupVolunteer()
+    {
     }
 }
